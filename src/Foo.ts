7f0ac936b357f20300877e8deb12dcf5ca39772b
Jed Mao <jedmao@outlook.com>
2016-07-02 01:58:52 -0500

    Introduce EditorConfig

--- src/Foo.ts
+++ src/Foo.ts
@@ -3,13 +3,13 @@ import Bar from './Bar';
 
 export default class Foo {
 
-  private config: any;
+	private config: any;
 
-  private bar: Bar;
+	private bar: Bar;
 
-  constructor(config:any) {
-    this.bar = new Bar;
-    this.config = assign({}, config);
-  }
+	constructor(config:any) {
+		this.bar = new Bar;
+		this.config = assign({}, config);
+	}
 
 }
