f039de0bff33b37dd8aff052cdf54a14dd0b4fc6
Jed Mao <jedmao@outlook.com>
2016-07-02 02:28:25 -0500

    Use tabs for indentation

--- webpack.config.ts
+++ webpack.config.ts
@@ -3,32 +3,32 @@ const nodeModulesPath = path.resolve(__dirname, 'node_modules');
 const webpack = require('webpack');
 
 export default {
-  entry: {
-    bundle: ['./entry'],
-  },
-  output: {
-    path: __dirname,
-    filename: '[name].js'
-  },
-  debug: true,
-  devtool: 'source-map',
-  module: {
-    loaders: [
-      {
-        test: /\.tsx?$/,
-        loaders: ['ts-loader'],
-        exclude: [/node_modules/, nodeModulesPath]
-      }
-    ]
-  },
-  externals: {
-    // don't bundle the 'react' npm package with our bundle.js
-    // but get it from a global 'React' variable
-    react: 'React'
-  },
-  plugins: [],
-  resolve: {
-    root: [path.resolve('./src')],
-    extensions: ['', '.ts', '.tsx', '.js']
-  }
+	entry: {
+		bundle: ['./entry'],
+	},
+	output: {
+		path: __dirname,
+		filename: '[name].js'
+	},
+	debug: true,
+	devtool: 'source-map',
+	module: {
+		loaders: [
+			{
+				test: /\.tsx?$/,
+				loaders: ['ts-loader'],
+				exclude: [/node_modules/, nodeModulesPath]
+			}
+		]
+	},
+	externals: {
+		// don't bundle the 'react' npm package with our bundle.js
+		// but get it from a global 'React' variable
+		react: 'React'
+	},
+	plugins: [],
+	resolve: {
+		root: [path.resolve('./src')],
+		extensions: ['', '.ts', '.tsx', '.js']
+	}
 };
